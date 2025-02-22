1f98154dbd59b1976d0bea8ffce343d7ee89ed1d
raydive <raywassmas@gmail.com>
2025-02-22 18:00:58 +0900

    Add Main

--- src/main/scala/com/github/raydive/Main.scala
+++ src/main/scala/com/github/raydive/Main.scala
@@ -1,7 +1,13 @@
 package com.github.raydive
 
+import com.github.raydive.bloomfilter.BloomFilter
+
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+    val filter = BloomFilter(1000, 0.01)
+    filter.set("hoge")
+
+    println(filter.mightContain("hoge"))
+    println(filter.mightContain("fuga"))
   }
 }
